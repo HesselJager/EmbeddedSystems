a690fc1d6d3913f49d3ecadd9a4e323f544c5616
Sebastiaan Holthuis <holthuissebastiaan@gmail.com>
2019-11-08 15:45:31 +0100

    Added main.py and .gitignore

---
+++ Interface/main.py
@@ -0,0 +1,29 @@
+import serial
+import serial.tools.list_ports
+import time
+
+class Main:
+    ports = []
+
+    def __init__(self):
+        self.ports = self.scanPorts()
+
+        for port in self.ports:
+            print port.device
+
+        ser = serial.Serial(port="COM3", baudrate=19200)
+
+        time.sleep(5)
+
+        if ser.isOpen():
+            test = 2
+            ser.write(bytes([test]))
+        else:
+            ser.open()
+
+        pass
+
+    def scanPorts(self):
+        return serial.tools.list_ports.comports()
+
+Main()
