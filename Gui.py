5ae79b012205a226e3ece6c0cb03381923d36ebd
LarsBuildy <45849799+LarsBuildy@users.noreply.github.com>
2019-11-08 15:39:16 +0100

    Add files via upload

--- Gui.py
+++ Gui.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from tkinter import ttk
 
+# functie die entry1 checkt
+def hitReturn(*args):
+    print("Data verzonden")
 
 
 # main window:
@@ -12,7 +15,7 @@ window.geometry("500x500")
 
 # de widget die de tabs implenmenteerd
 tabControl = ttk.Notebook(window)
-tabControl.grid(row =1, column = 0, columnspan = 50, rowspan= 49, sticky = "NESW")
+tabControl.grid(row =1, column = 0, sticky = "NESW")
 
 tab1 = ttk.Frame(tabControl)
 tabControl.add(tab1, text = "Settings")
@@ -26,9 +29,15 @@ tabControl.add(tab3, text = "Temperatuursensor")
 tab4 = ttk.Frame(tabControl)
 tabControl.add(tab4, text = "Ultrasoonsensor")
 
+label1 = Label(tab1, text = "Voer temperatuur in:")
+label1.grid(row = 1, column =0, sticky = "W")
+
+# Het veld dat de data verzend naar de arduino
+entry1 = Entry(tab1)
+entry1.grid(row = 3, column = 0, sticky = "W")
+entry1.bind("<Return>", hitReturn)
 
 
-#maakt de verschillende tabs
 
 
 
