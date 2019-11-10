f7b2b86c14c28f7f8416d8b4fe24bfe8191247fe
Sebastiaan Holthuis <holthuissebastiaan@gmail.com>
2019-11-10 21:00:18 +0100

    teawtawtaewt

--- Interface/port_thread.py
+++ Interface/port_thread.py
@@ -2,13 +2,18 @@ import threading
 import serial
 import time
 
-class PortThread:
+class PortThread (threading.Thread):
 
     def __init__(self, port):
+        threading.Thread.__init__(self)
         self.port = port
-        self.ser = serial.Serial(port='COM3', baudrate=19200, timeout=5)
+        self.ser = serial.Serial(port=port, baudrate=9600, timeout=5)
 
-        while True:
-            self.ser.readline()
-            time.sleep(2)
+    def readCommand(self):
+        print 'hey'
 
+    def run(self):
+        length = ord(self.ser.read(1).decode('utf-8'))  # Get command length
+        commandString = self.ser.read(length).decode('utf-8')  # Get entire command with recieved length
+
+        threading.Timer(0.01, self.readCommand).start()
