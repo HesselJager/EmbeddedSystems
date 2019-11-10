6026720e96db44ebe3aabbc4793edc8f004206b3
Sebastiaan Holthuis <holthuissebastiaan@gmail.com>
2019-11-10 17:27:39 +0100

    joost

--- Interface/port_thread.py
+++ Interface/port_thread.py
@@ -8,10 +8,22 @@ class PortThread (threading.Thread):
         threading.Thread.__init__(self)
         self.port = port
         self.ser = serial.Serial(port=port, baudrate=19200, timeout=5)
+        time.sleep(2)
 
     def readCommand(self):
         print 'hey'
 
     def run(self):
-        print self.ser.readline()
-        threading.Timer(0.01, self.readCommand).start()
+        while True:
+            print 'true'
+            self.ser.write(b'\x02')
+            time.sleep(8)
+
+            line = self.ser.read()
+
+            # only show bytes with content, not the empty ones
+            if (line != b''):
+                # make sure to use int.from_bytes(line, "big")
+                # otherwise it will print something like b'\xa4'
+                print(int.from_bytes(line, "big"))
+                print(line)
