7c8f25a9d99b9a4175cbf3cd0103b2643e1a41cc
Sebastiaan Holthuis <holthuissebastiaan@gmail.com>
2019-11-10 17:33:42 +0100

    testje

--- Interface/port_thread.py
+++ Interface/port_thread.py
@@ -2,18 +2,13 @@ import threading
 import serial
 import time
 
-class PortThread (threading.Thread):
+class PortThread:
 
     def __init__(self, port):
-        threading.Thread.__init__(self)
         self.port = port
-        self.ser = serial.Serial(port=port, baudrate=19200, timeout=5)
+        self.ser = serial.Serial(port="COM3", baudrate=19200, timeout=5)
         time.sleep(2)
 
-    def readCommand(self):
-        print 'hey'
-
-    def run(self):
         while True:
             print 'true'
             self.ser.write(b'\x02')
@@ -27,3 +22,5 @@ class PortThread (threading.Thread):
                 # otherwise it will print something like b'\xa4'
                 print(int.from_bytes(line, "big"))
                 print(line)
+
+            print line
