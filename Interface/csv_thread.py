0a27065460b61e805731c082952141587152b453
Sebastiaan Holthuis <holthuissebastiaan@gmail.com>
2019-11-11 13:40:43 +0100

    csv gemaakt

---
+++ Interface/csv_thread.py
@@ -0,0 +1,24 @@
+import threading
+import io
+import numpy as np
+import csv
+
+class CsvThread (threading.Thread):
+
+    def __init__(self, main):
+        self.main = main
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.update()
+
+    def update(self):
+        with open('test_data.csv', 'rb') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=';')
+            results = []
+
+            for row in csv_reader:
+                results.append(row)
+                self.main.csv_data = results
+
+        threading.Timer(1, self.update).start()
